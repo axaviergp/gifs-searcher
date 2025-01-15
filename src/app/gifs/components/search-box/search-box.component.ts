e70b9c9b8608efb0296601193e9571fe4cd270a3
Xavier Gomez <axaviergp@gmail.com>
2025-01-15 16:22:15 -0600

    First commit

---
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -0,0 +1,29 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'gifs-search-box',
+  standalone: false,
+
+  template: `
+    <h5>Buscar</h5>
+    <input
+      type="text"
+      class="form-control"
+      placeholder="Buscar Gifs ..."
+      #txtTagInput
+      (keyup.enter)="searchTag()"
+    >
+  `,
+  styleUrl: './search-box.component.css'
+})
+export class SearchBoxComponent {
+  @ViewChild('txtTagInput')
+  public tagInput!: ElementRef<HTMLInputElement>;
+
+  // searchTag(newTag: string) {
+  searchTag() {
+    const newTag = this.tagInput.nativeElement.value;
+    console.log(newTag);
+  }
+
+}
